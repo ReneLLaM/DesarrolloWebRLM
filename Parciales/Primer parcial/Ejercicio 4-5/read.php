4d5a454b59d9f5f4dcfeb7b5155b5ed26211c90c
Rene Llanos <renellanosmachuca@gmail.com>
2025-04-23 02:08:04 -0400

    Practica 8-9

--- Parciales/Primer parcial/Ejercicio 4-5/read.php
+++ Parciales/Primer parcial/Ejercicio 4-5/read.php
@@ -24,10 +24,20 @@ require("verificarsesion.php");
 <a href="cerrar.php">Cerrar Sesion</a>
 
 <?php
+
+
+
 include("conexion.php");
+if((!empty($_GET['orden']))){
+    $orden = $_GET['orden'];
+}else{
+    $orden = "titulo";
+    $asc = "ASC";
+}
 
 $sql="SELECT libros.id,imagen,titulo,autor,editoriales.editorial as editorial,anio FROM libros 
-LEFT JOIN editoriales on editoriales.id = libros.id";  
+LEFT JOIN editoriales on editoriales.id = libros.id
+ORDER BY $orden $asc";  
 
 $resultado=$con->query($sql);
 
@@ -36,10 +46,10 @@ $resultado=$con->query($sql);
     <thead>
         <tr>
             <th width="100px">Imagen</th>
-            <th width="100px">Titulo</th>
-            <th width="100px">Titulo</th>
-            <th width="100px">editorial</th>
-            <th width="100px">año</th>
+            <th width="100px"><a href="read.php?orden=titulo&asc= <?php if($orden == "titulo" && $asc == 'ASC'){echo 'DESC';}else if( $orden == "titulo" && $asc == 'DESC'){echo 'ASC';}?>">Titulo</a></th> 
+            <th width="100px"><a href="read.php?orden=autor&asc= <?php if($orden == "autor" && $asc == 'ASC'){echo 'DESC';}else if( $orden == "autor" && $asc == 'DESC'){echo 'ASC';}?>">Autor</a></th>
+            <th width="100px"><a href="read.php?orden=editoriales.editorial&asc= <?php if($orden == "editoriales.editorial" && $asc == 'ASC'){echo 'DESC';}else if( $orden == "editoriales.editorial" && $asc == 'DESC'){echo 'ASC';}?>">Editorial</a></th>
+            <th width="100px"><a href="read.php?orden=anio&asc= <?php if($orden == "anio" && $asc == 'ASC'){echo 'DESC';}else if( $orden == "anio" && $asc == 'DESC'){echo 'ASC';}?>">Año</a></th>
 
            <?php if($_SESSION['nivel']==1){?>
             <th>Operaciones</th>
